101b86b8db87e56d96f806df194b694d84b85182
Hy <hyhuynh222@gmail.com>
2024-06-11 08:46:00 -0700

    + comment

--- src/main/java/genspark/pbc/OnlineBookstore/OnlineBookstoreApplication.java
+++ src/main/java/genspark/pbc/OnlineBookstore/OnlineBookstoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookstoreApplication {
-
+	// Run the backend
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookstoreApplication.class, args);
 	}
